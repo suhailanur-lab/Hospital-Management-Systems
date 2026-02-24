7aac1e891ed3d11c8b78e135d326b7f42d6de857
suhailanur-lab <suhaila.nur@strathmore.edu>
2026-02-24 15:19:21 +0300

    .

--- Doctor.java
+++ Doctor.java
@@ -1,19 +1,23 @@
+package hospital;
+
 public class Doctor extends Person implements Treatable {
 
     private String specialty;
 
     public Doctor(String name, int age, String specialty) {
-        super(name, age);
-        this.specialty = specialty;
+        super(name, age); // using super
+        this.specialty = specialty; // using this
     }
 
     @Override
     public void showInfo() {
-        System.out.println("Doctor: " + this.getName() + ", Specialty: " + specialty);
+        System.out.println("Doctor: " + this.getName() +
+                ", Age: " + this.getAge() +
+                ", Specialty: " + specialty);
     }
 
     @Override
     public void treat() {
-        System.out.println("Dr. " + this.getName() + " is treating patients");
+        System.out.println("Dr. " + this.getName() + " is treating patients.");
     }
 }
