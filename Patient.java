e9742797654f70bfe7430ffb69ddd121e50fd501
suhailanur-lab <suhaila.nur@strathmore.edu>
2026-02-24 13:52:28 +0300

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,23 @@
+public class Patient extends Person implements Billable {
+
+    private double totalBill;
+
+    public Patient(String name, int age) {
+        super(name, age);
+        this.totalBill = 0;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Patient: " + this.getName() + ", Bill: $" + totalBill);
+    }
+
+    @Override
+    public double getBill() {
+        return this.totalBill;
+    }
+
+    public void addToBill(double amount) {
+        this.totalBill += amount;
+    }
+}
